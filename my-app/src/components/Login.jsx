1884d2394c8326d1295dc6991617259743bdf6a3
iamveerendragangwar <128514719+helovirendra40@users.noreply.github.com>
2024-10-19 14:25:54 +0530

    Update Login.jsx

--- my-app/src/components/Login.jsx
+++ my-app/src/components/Login.jsx
@@ -13,21 +13,28 @@ function Login({ onLogin }) {
   };
 
   return (
-    <div className='d-flex justify-content-center align-items-center h-100vh'>
+    <div className='d-flex position-relative justify-content-center align-items-center h-100vh'>
+      <div className="card1"></div>
+        <div className="card2"></div>
+        <div className="card3"></div>
+        <div className="card4"></div>
       <div className='forms'>
+
+        
+      
         <h2 className='mb-3'>Login</h2>
         <form onSubmit={handleSubmit}>
           <input className='form-control mb-3'
             type="text"
-            placeholder="Username"
+            placeholder="Name"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)} required
           />
           <input  className='form-control mb-3'
             type="password"
             placeholder="Password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)} required
           />
           <p>don't have an account <Link to={'/register'}>Register</Link> </p>
           <button className='btn btn-primary' type="submit">Login</button>
